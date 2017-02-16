13cfbe26590be6262c86283a001a974df2a0cc08
SuguruOoki <suguru.oki.programming@gmail.com>
2017-02-16 19:31:12 +0900

    メッセージの画面出力の調整完了。

--- index.js
+++ index.js
@@ -8,9 +8,33 @@ var rangeVal = utils.decode_range(range);
 var len = utils.sheet_to_json(worksheet).length;
 var content = JSON.stringify(utils.sheet_to_json(worksheet));
 
+var first = "①Pepperの発話内容"
+var second = "②反応する言葉"
+var third = "③お客さんの言葉に対するPepperの反応(Pepper言語で)"
+var forth = "④反応する言葉"
+var fifth = "⑤お客さんの言葉に対するPepperの反応"
+
 for (var i = 0; i < utils.sheet_to_json(worksheet).length; i++) {
 
-    document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i]["①Pepperの発話内容"])+"<br><br>");
+    //Pepperの発話内容をJSONから取り出すS
+    if(typeof(utils.sheet_to_json(worksheet)[i][first]) !== "undefined"){
+        document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i][first])+"<br>");
+        document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i][second])+"<br>");
+        document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i][third])+"<br>");
+        if(typeof(utils.sheet_to_json(worksheet)[i][forth]) !== "undefined"){
+            document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i][forth])+"<br>");
+        }
+        else if(typeof(utils.sheet_to_json(worksheet)[i+1][first]) === "undefined"){
+            document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i+1][second])+"<br>");
+            document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i+1][third])+"<br>");
+        }
+        if(typeof(utils.sheet_to_json(worksheet)[i][fifth]) !== "undefined"){
+            document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i][fifth])+"<br><br><br>");
+        }
+
+        document.write("<br><br>")
+    }
+    // document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i])+"<br><br><br>");
 
 };
 
