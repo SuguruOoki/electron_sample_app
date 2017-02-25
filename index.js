a4a9b50b1fb67d76e825590020882cae03c348a7
SuguruOoki <suguru.oki.programming@gmail.com>
2017-02-25 16:16:37 +0900

    分岐出力以外の出力完成

--- index.js
+++ index.js
@@ -7,36 +7,97 @@ var range = worksheet['!ref'];
 var rangeVal = utils.decode_range(range);
 var len = utils.sheet_to_json(worksheet).length;
 var content = JSON.stringify(utils.sheet_to_json(worksheet));
+var text = "";
+var first = "①Pepperの発話内容";
+var second = "②反応する言葉";
+var third = "③お客さんの言葉に対するPepperの反応(Pepper言語で)";
+var forth = "④反応する言葉";
+var fifth = "⑤お客さんの言葉に対するPepperの反応";
+var start_date = "開始日";
+var end_date = "終了日";
 
-var first = "①Pepperの発話内容"
-var second = "②反応する言葉"
-var third = "③お客さんの言葉に対するPepperの反応(Pepper言語で)"
-var forth = "④反応する言葉"
-var fifth = "⑤お客さんの言葉に対するPepperの反応"
+// 重複を削除したリスト
+//var list = a.filter(function (x, i, self) {return self.indexOf(x) === i;});
 
-for (var i = 0; i < utils.sheet_to_json(worksheet).length; i++) {
+date_dic = {};
+start_end_date = []; 
 
-    //Pepperの発話内容をJSONから取り出すS
+for (var i = 0; i < len; i++) {
     if(typeof(utils.sheet_to_json(worksheet)[i][first]) !== "undefined"){
-        document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i][first])+"<br>");
-        document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i][second])+"<br>");
-        document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i][third])+"<br>");
-        if(typeof(utils.sheet_to_json(worksheet)[i][forth]) !== "undefined"){
-            document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i][forth])+"<br>");
-        }
-        else if(typeof(utils.sheet_to_json(worksheet)[i+1][first]) === "undefined"){
-            document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i+1][second])+"<br>");
-            document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i+1][third])+"<br>");
-        }
-        if(typeof(utils.sheet_to_json(worksheet)[i][fifth]) !== "undefined"){
-            document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i][fifth])+"<br><br><br>");
-        }
-
-        document.write("<br><br>")
+            date_dic[utils.sheet_to_json(worksheet)[i][first]] = [utils.sheet_to_json(worksheet)[i][start_date],utils.sheet_to_json(worksheet)[i][end_date]];
+            document.write(dic[utils.sheet_to_json(worksheet)[i][first]]+"<br>");
     }
-    // document.write(JSON.stringify(utils.sheet_to_json(worksheet)[i])+"<br><br><br>");
+}
 
+var arr;
+
+for(key in date_dic){
+  arr.append(date_dic[key]);
+  document.write(key+":"+date_dic[key]);
+}
+
+
+
+var result = Object.keys(date_dic).filter( (key) => { return date_dic[key] === 23,23});
+
+//document.write(result);
+
+var arrayGetValues = function(array) {
+    var values = [];
+ 
+    if (array) {
+        for (var key in array) {
+            values.push(array[key]);
+        }
+    }
+ 
+    return values;
 };
+
+var arrayValues = arrayGetValues(arr);
+
+document.write(arrayValues);
+
+var re = Array.from(new Set(arrayValues));
+document.write(re);
+
+var count = 0;
+//text = text + "length:"+len+"\n\n");
+for (var i = 0; i < len; i++) {
+
+    //Pepperの発話内容をJSONから取り出す
+    //最初に判断するのは一つ目の会話があるかどういか
+    //あるなら次の処理をする
+    if(typeof(utils.sheet_to_json(worksheet)[i][first]) !== "undefined"){
+        text = text + "\n\n";
+        count += 1;
+        text = text + "proposal:%conversation_01_"+('000'+count).slice(-3)+"\n";   //数値は指定の桁数で固定する
+        //text = text + "    "+"\\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation\n    "+JSON.stringify(utils.sheet_to_json(worksheet)[i][first]).replace(/["']+/g, '')+"\n    \\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation_end\n");
+        text = text + "    "+"\\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation\n       "+JSON.stringify(utils.sheet_to_json(worksheet)[i][first]).replace(/["']+/g, '')+"\n    \\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation_end\n";
+        //最初が空欄ではなく２、３番目は空欄の場合
+        if(typeof(utils.sheet_to_json(worksheet)[i][second]) === "undefined" && typeof(utils.sheet_to_json(worksheet)[i][third]) === "undefined"){
+            text = text + "\n\n";
+        }
+        //最初が空欄ではなく２、３番目がきっちりあった場合
+        else if(typeof(utils.sheet_to_json(worksheet)[i][second]) !== "undefined" && typeof(utils.sheet_to_json(worksheet)[i][third]) !== "undefined"){
+            // text = text + "\n        u1:(\"{*}"+JSON.stringify(utils.sheet_to_json(worksheet)[i][second]).replace(/["']+/g, '')+" {*} $SekkyakuPepper/Scene<>conversation $SekkyakuPepper/Scene<>speech\""+")\n");
+            // text = text + "\n                \\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation\n                "+JSON.stringify(utils.sheet_to_json(worksheet)[i][third]).replace(/["']+/g, '')+"\n            \\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation_end\n");
+            text = text + "\n        u1:(\"{*}"+JSON.stringify(utils.sheet_to_json(worksheet)[i][second]).replace(/["']+/g, '')+" {*} $SekkyakuPepper/Scene<>conversation $SekkyakuPepper/Scene<>speech\""+")\n";
+            text = text + "\n            \\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation\n                "+JSON.stringify(utils.sheet_to_json(worksheet)[i][third]).replace(/["']+/g, '')+"\n            \\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation_end\n";
+        }
+    }
+    //最初が空欄かつ二番目が空欄でない場合
+    else if(typeof(utils.sheet_to_json(worksheet)[i][first]) === "undefined" && typeof(utils.sheet_to_json(worksheet)[i][second]) !== "undefined"){
+        text = text + "\n        u1:(\"{*}"+JSON.stringify(utils.sheet_to_json(worksheet)[i][second]).replace(/["']+/g, '')+" {*} $SekkyakuPepper/Scene<>conversation $SekkyakuPepper/Scene<>speech\""+")\n";
+        text = text + "\n            \\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation\n                "+JSON.stringify(utils.sheet_to_json(worksheet)[i][third]).replace(/["']+/g, '')+"\n            \\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation_end\n";
+    }
+    //最初と２番目が空欄の場合
+    else if(typeof(utils.sheet_to_json(worksheet)[i][first]) === "undefined" && typeof(utils.sheet_to_json(worksheet)[i][second]) === "undefined"　&& typeof(utils.sheet_to_json(worksheet)[i][third]) !== "undefined"){
+        text = text + "\n                \\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation\n                "+JSON.stringify(utils.sheet_to_json(worksheet)[i][third]).replace(/["']+/g, '')+"\n            \\vct=140\\ \\rspd=100\\ $SekkyakuPepper/Scene=conversation_end\n";
+    }
+
+}
+
 
 
 function textSave(name, text) {
@@ -47,8 +108,11 @@ function textSave(name, text) {
     link.click();
 }
 
-// textSave('myfile',content);
-// document.write(content);
+//textSave('myfile',text);
+
+
+
+// text = text + content);
 
 // var a = document.createElement('a');
 // a.textContent = 'export';
